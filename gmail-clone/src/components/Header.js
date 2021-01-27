f6e75849295a2360b2cb7e964806d5a1aa275d7e
EYE MOTION <eyemotiongamestudio@gmail.com>
2021-01-27 15:48:52 +0400

    update

---
+++ gmail-clone/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="header">
+            <img src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r2.png" alt=""/>
+        </div>
+    )
+}
+
+export default Header
---
+++ gmail-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB_E6DQulK7drnpHPsK786UFTN0hC2caDA",
+    authDomain: "clone-13bff.firebaseapp.com",
+    projectId: "clone-13bff",
+    storageBucket: "clone-13bff.appspot.com",
+    messagingSenderId: "174076373815",
+    appId: "1:174076373815:web:d1aa32fbee4f81c92048b2"
+  };
